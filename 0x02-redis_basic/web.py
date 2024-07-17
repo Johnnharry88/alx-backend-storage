68813222df9d19a749ac9b7ca22ccfc5fb3c7a92
Johnharry88 <harryjohnwhye@gmail.com>
2024-07-17 14:10:25 +0100

    Redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,7 +13,7 @@ def url_access_count(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(url: str) -> str:
         """Checks out for wrapped function"""
-        alx = redis.Redis()        
+        alx = redis.Redis()
         alx.incr(f'count:{url}')
         cache_p = alx.get(f'cached:{url}')
         if cache_p:
@@ -29,7 +29,3 @@ def get_page(url: str) -> str:
     """Get the HTML content of a particular page"""
     res = requests.get(url)
     return res.text
-
-
-if __name__ == "__main__":
-    get_page("http://slowwly.robertomurray.co.uk")
